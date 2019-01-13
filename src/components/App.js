da61ef6b915299ba3042375d8b16f799b07c7bf2
unknown <megrumble@gmail.com>
2019-01-13 10:28:52 -0500

    initialized state in app component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    //initialize state with images as an array so we can later utilize array functions
+    state = { images: [] }
+    //use async/await to allow asynchronous activity
+    //this needs to be an arrow function in order to bind to this keyword
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: term
@@ -12,8 +16,10 @@ class App extends React.Component {
                 Authorization: 'Client-ID d157da64a146c90314bff6959e69ac47438a34dbf56db5073520db421815c851'
             }
         });
-        console.log(response.data.results);
+        //setState to return an array of images
+        this.setState ({ images: response.data.results });
     }
+    //this will render whenever state is initialized or updated
     render() {
         return (
             <div
@@ -22,6 +28,7 @@ class App extends React.Component {
                 marginTop: '10px'
             }}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         );
     };
